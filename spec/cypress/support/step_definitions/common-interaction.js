37d78f9c9eee259b332d1459d29a457ece8e1db6
Callum Macrae <callum.macrae@digital.education.gov.uk>
2021-03-23 12:35:03 +0000

    Add percy setup + initial test

--- spec/cypress/support/step_definitions/common-interaction.js
+++ spec/cypress/support/step_definitions/common-interaction.js
@@ -61,3 +61,11 @@ Then("{string} should not exist", (element) => {
 Then("the page should be accessible", () => {
   cy.checkA11y();
 });
+
+Then("percy should be sent snapshot", () => {
+  cy.percySnapshot();
+});
+
+Then("percy should be sent snapshot called {string}", (name) => {
+  cy.percySnapshot(name);
+});
